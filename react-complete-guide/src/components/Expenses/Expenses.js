3f0179b47f984bb601dda5aa3d18673ca406c0c7
mhernandez <mhernandez@xumak.com>
2021-06-11 22:42:42 -0600

    add Expense FIlter

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,11 +1,23 @@
+import {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 function Expenses(props) {
-    const expenses = props.expenses;
-    return (
+  const expenses = props.expenses;
+  const [filteredYear, setFilteredYear] = useState('2021');
+  const onSelectHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
+  return (
+    <div>
       <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={onSelectHandler}
+        />
         <ExpenseItem
           title={expenses[0].title}
           amount={expenses[0].amount}
@@ -27,7 +39,8 @@ function Expenses(props) {
           date={expenses[3].date}
         />
       </Card>
-    );
+    </div>
+  );
 }
 
 export default Expenses;
