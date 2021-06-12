95bfb7f39fa8720e45bf1ab2fc4cfab1373797ef
mhernandez <mhernandez@xumak.com>
2021-06-12 11:57:04 -0600

    add new Expense feature

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -18,26 +18,13 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={onSelectHandler}
         />
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
+        {expenses.map((ex) => (
+          <ExpenseItem
+            key={ex.id}
+            title={ex.title}
+            amount={ex.amount}
+            date={ex.date} />
+        ))}
       </Card>
     </div>
   );
