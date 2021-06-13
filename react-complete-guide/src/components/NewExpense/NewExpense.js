a3ce00bf437d687f91e18bd080595f3513a43f61
mhernandez <mhernandez@xumak.com>
2021-06-13 09:35:16 -0600

    Conditional form

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,8 +1,11 @@
 
+import { useState } from 'react';
 import './NewExpense.css';
 import ExpensesForm from './ExpensesForm';
 
 const NewExpense = (props) => {
+
+    const [showForm, setShowForm] = useState(false);
 
     const onSaveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -11,11 +14,21 @@ const NewExpense = (props) => {
         };
         console.log(expenseData);
         props.onAddExpense(expenseData);
-
+        setShowForm(false);
     }
 
+    const onCancelHandler = cancelData => {
+        setShowForm(false)
+    }
+
+    const content = showForm ? (
+      <ExpensesForm onSaveExpenseData={onSaveExpenseDataHandler} onCancel={onCancelHandler} />
+    ) : (
+      <button onClick={() => setShowForm(true)}> Add New Expense </button>
+    );
+
     return <div className="new-expense">
-        <ExpensesForm onSaveExpenseData={ onSaveExpenseDataHandler }/>
+        {content}
     </div>
 };
 
